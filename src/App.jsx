60d13d7945ed523d040a53726ac1012b36ee0c10
Kenref <107287396+Kenref@users.noreply.github.com>
2023-11-13 10:42:36 +1100

    moved images to public folder

--- src/App.jsx
+++ src/App.jsx
@@ -50,29 +50,29 @@ export default function App() {
 	const contactRef = useRef(null);
 	const skills = [
 		{ src: "/HTML-Icon.png", alt: "HTML logo", title: "HTML" },
-		{ src: "./assets/CSS-Icon.png", alt: "CSS logo", title: "CSS" },
+		{ src: "/CSS-Icon.png", alt: "CSS logo", title: "CSS" },
 		{
-			src: "/public/JS-Icon.png",
+			src: "/JS-Icon.png",
 			alt: "JavaScript logo",
 			title: "JavaScript",
 		},
-		{ src: "assets/React-Icon.png", alt: "React logo", title: "React" },
-		{ src: "src/assets/Nodejs-Icon.png", alt: "Node logo", title: "Node.js" },
-		{ src: "src/assets/Vite-Icon.png", alt: "Vite logo", title: "Vite" },
-		{ src: "src/assets/Jest-Icon.png", alt: "Jest logo", title: "Jest" },
-		{ src: "src/assets/Vitest-Icon.png", alt: "Vitest logo", title: "Vitest" },
+		{ src: "/React-Icon.png", alt: "React logo", title: "React" },
+		{ src: "/Nodejs-Icon.png", alt: "Node logo", title: "Node.js" },
+		{ src: "/Vite-Icon.png", alt: "Vite logo", title: "Vite" },
+		{ src: "/Jest-Icon.png", alt: "Jest logo", title: "Jest" },
+		{ src: "/Vitest-Icon.png", alt: "Vitest logo", title: "Vitest" },
 		{
-			src: "src/assets/Bootstrap-Icon.png",
+			src: "/Bootstrap-Icon.png",
 			alt: "Bootstrap logo",
 			title: "Bootstrap",
 		},
 		{
-			src: "src/assets/Python-Icon.png",
+			src: "/Python-Icon.png",
 			alt: "Python logo",
 			title: "Python",
 		},
 		{
-			src: "src/assets/Github-Icon.png",
+			src: "/Github-Icon.png",
 			alt: "Github logo",
 			title: "Github",
 		},
@@ -81,7 +81,7 @@ export default function App() {
 	const projects = [
 		{
 			title: "Memory Card Game",
-			src: "src/assets/memory-mockup.png",
+			src: "/memory-mockup.png",
 			alt: "Memory Card Game mockup",
 			description:
 				"Memory card game built with the RESTful Poke API. Objective is to click on all unique cards until all have been selected. Able to select from 4 diificulties and play with different cards each time",
@@ -91,7 +91,7 @@ export default function App() {
 		},
 		{
 			title: "Calculator",
-			src: "src/assets/calculator-mockup.png",
+			src: "/calculator-mockup.png",
 			alt: "Calculator",
 			description:
 				"A working calculator built using plain Javascript with keyboard functionality and the ability to do consecutive calculations.",
